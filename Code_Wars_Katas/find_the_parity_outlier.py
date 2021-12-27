1d1f3016997b3101566d512f866833634ab2e9cd
matthewfduffy <matthewfduffy@gmail.com>
2021-12-27 10:41:23 -0600

    updating Code War challenge descriptions

--- Code_Wars_Katas/find_the_parity_outlier.py
+++ Code_Wars_Katas/find_the_parity_outlier.py
@@ -3,10 +3,10 @@ You are given an array (which will have a length of at least 3, but could be ver
 
 # Examples:
 [2, 4, 0, 100, 4, 11, 2602, 36]
-Should return: 11 (the only odd number)
+    Should return: 11 (the only odd number)
 
 [160, 3, 1719, 19, 11, 13, -21]
-Should return: 160 (the only even number)
+    Should return: 160 (the only even number)
 """
 def find_outlier(integers):
     tmp_list = []
