850cfc749899ef66cf0733d96d4eec38a03ab300
matthewfduffy <matthewfduffy@gmail.com>
2024-05-04 11:23:43 -0500

    Adding count_by_x challenge

---
+++ Code_Wars_Katas/count_by_x.py
@@ -0,0 +1,22 @@
+"""
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+Examples
+
+count_by(1,10) #should return [1,2,3,4,5,6,7,8,9,10]
+count_by(2,5) #should return [2,4,6,8,10]
+"""
+
+def count_by(x, n):
+    return_array = [x]
+    for i in range(n-1):
+        return_array.append(return_array[-1] + x)
+    return return_array
+
+
+print(count_by(1,10))
+print(count_by(2,5))
+    
