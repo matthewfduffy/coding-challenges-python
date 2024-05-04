30e10647bce1861e7cf85544ff37db3bf692a197
matthewfduffy <matthewfduffy@gmail.com>
2024-05-04 15:24:40 -0500

    adding alternate option for simple_multiplication.py

--- Code_Wars_Katas/sentence_smash.py
+++ Code_Wars_Katas/sentence_smash.py
@@ -3,6 +3,7 @@ Write a function that takes an array of words and smashes them together into a s
 
 ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 """
+# Option 1
 def smash(words):
     new_string = ""
     for word in words:
@@ -14,3 +15,7 @@ test_b = ["", 'hello', 'world', 'this', 'is', 'great']
 
 print(smash(test_a))
 print(smash(test_b))
+
+# Option 2
+def smash(words):
+    return " ".join(words)
