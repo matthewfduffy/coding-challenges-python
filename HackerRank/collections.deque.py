ad0041ccb50def38656550c4d3dd3e4d2ed0686b
matthewfduffy <matthewfduffy@gmail.com>
2024-06-08 14:25:29 -0500

    Adding collections.deque

--- HackerRank/collections.deque.py
+++ HackerRank/collections.deque.py
@@ -3,7 +3,6 @@ A deque is a double-ended queue. It can be used to add or remove elements from b
 
 Deques support thread safe, memory efficient appends and pops from either side of the deque with approximately the same O(1) performance in either direction.
 
-[Example]
 
 [TASK]
 Perform append, pop, popleft and appendleft methods on an empty deque d
@@ -26,3 +25,28 @@ popleft
 
 [Sample Output]
 1 2
+"""
+from collections import deque
+
+n = int(input())
+dq = deque()
+ 
+
+for _ in range(n):
+    input_str = input().split()
+    action = input_str[0]
+
+    if len(input_str) > 1:
+        val = int(input_str[-1])
+
+    match action:
+        case "append":
+            dq.append(val)
+        case "appendleft":
+            dq.appendleft(val)
+        case "pop":
+            dq.pop()
+        case "popleft":
+            dq.popleft()
+
+print(*dq)
