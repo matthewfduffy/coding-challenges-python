05c1255da3dfd80b0e0862ffe7ff924e2664ef4f
matthewfduffy <matthewfduffy@gmail.com>
2024-06-08 14:53:45 -0500

    updating recursive option to grasshopper_summation.py

--- Code_Wars_Katas/grasshopper_summation.py
+++ Code_Wars_Katas/grasshopper_summation.py
@@ -7,10 +7,21 @@ For example (Input -> Output):
 2 -> 3 (1 + 2)
 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 """
+# Option 1
 def sum_to_zero(n):
 	result = 0
 	for num in range(n, 0, -1):
 		result += num
 	return result
 
-print(sum_to_zero(8))
+# print(sum_to_zero(8))
+
+# Option 2 - Recursion
+def sum_to_num(n):
+    if n == 1:
+        return n
+    else:
+		# print(f"The current value of n is: {n}")
+	    return n + sum_to_num(n-1)
+
+print(sum_to_num(8))
