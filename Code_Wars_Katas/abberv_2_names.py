5291ed20633a18a7b6cffec32e6ce6bd657ce1d8
matthewfduffy <matthewfduffy@gmail.com>
2024-06-09 11:54:59 -0500

    adding alternate options to abbrev_2_names.py

--- Code_Wars_Katas/abberv_2_names.py
+++ Code_Wars_Katas/abberv_2_names.py
@@ -14,9 +14,20 @@ def abbrev_name(name):
     initials = name.split(" ")
     return str(initials[0][0] + "." + initials[1][0]).upper()
 
+# Option 2
+def abbrev_name2(name):
+    first, last = name.upper().split(' ')
+    return first[0] + '.' + last[0]
+
+# Option 3
+def abbrev_name3(name):
+    return '.'.join(w[0] for w in name.split()).upper()
 
 
-
+# Option 4
+def abbrev_name4(name):
+    initials = name.split()
+    return f"{initials[0][0]}.{initials[1][0]}".upper()
 
 test_name = "mike jones"
 print(abbrev_name(name))
